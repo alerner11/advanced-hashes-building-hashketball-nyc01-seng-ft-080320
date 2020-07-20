b27fe8b505518fcde539306d1a01a1de9fcc702c
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-20 20:03:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,17 @@
-# Write your code here!
+def game_hash
+  built_hash = {
+    home: {
+      team name: "",
+      colors: [],
+      players: [
+        {}
+      ]
+    },
+    away: {
+      team name: "",
+      colors: [],
+      players: [
+      ]
+    }
+  }
+end
